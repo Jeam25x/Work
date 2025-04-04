5c006d20fd6aa9af324c197a200f22f1d39134d1
Jeam25x <jpcm0820@gmail.com>
2025-04-03 20:48:04 -0500

    Add files via upload

---
+++ SOLID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solid;
+
+/**
+ *
+ * @author C2A503-20
+ */
+public class SOLID {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
